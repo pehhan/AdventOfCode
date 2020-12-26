a76aeb32ce0f9063d3b751bc53b3b620d8a99c1f
Pehr Hansson <pehr.hansson@parkster.se>
2020-12-26 18:10:37 +0100

    Day 23 Part 2 2020.

--- src/main/kotlin/adventofcode/year2020/day23/Day23.kt
+++ src/main/kotlin/adventofcode/year2020/day23/Day23.kt
@@ -1,55 +1,94 @@
 package adventofcode.year2020.day23
 
+import java.lang.IllegalArgumentException
+
 typealias Cup = Int
+typealias Cups = Map<Cup, Cup>
 
 object Task1 {
     fun getCupLabels(input: String): String {
-        return getCupLabels(input, 100)
+        val inputCups = input.map { it.toString().toInt() }
+        val cups = getCups(createInitialCupsMap(inputCups), inputCups[0], 100)
+        return getStringRepresentation(cups)
     }
 
-    private fun getCupLabels(input: String, iterations: Int): String {
-        val cups = input.map { it.toString().toInt() }.toMutableList()
-        val cupsSize = cups.size
+    private fun createInitialCupsMap(inputCups: List<Int>): Cups {
+        val cups = mutableMapOf<Int, Int>()
 
-        var currentCupIndex = 0
-
-        repeat(iterations) {
-            val currentCup = cups[currentCupIndex]
-
-            val cupsToRemoveIndex1 = (currentCupIndex + 1) % cups.size
-            val cupsToRemoveIndex2 = (currentCupIndex + 2) % cups.size
-            val cupsToRemoveIndex3 = (currentCupIndex + 3) % cups.size
-
-            val cupsToRemove = listOf(cups[cupsToRemoveIndex1], cups[cupsToRemoveIndex2], cups[cupsToRemoveIndex3])
-            cups.removeAll(cupsToRemove)
-
-            val destinationIndex = findDestinationIndex(cups, currentCup, cupsSize)
-            cups.addAll(destinationIndex, cupsToRemove)
-
-            currentCupIndex = (cups.indexOf(currentCup) + 1) % cups.size
+        for (i in 0 until inputCups.size - 1) {
+            cups[inputCups[i]] = inputCups[i + 1]
         }
 
-        return (cups.subList(cups.indexOf(1) + 1, cups.size) + cups.subList(0, cups.indexOf(1))).joinToString("")
+        // Fix last item
+        cups[inputCups[inputCups.size - 1]] = inputCups[0]
+
+        return cups
     }
 
-    private fun findDestinationIndex(cups: List<Cup>, currentCup: Cup, cupsSize: Int): Int {
-        var destinationValue = getDestinationValue(currentCup - 1, cupsSize)
-        var destinationCup = cups.firstOrNull { it == destinationValue }
-
-        while (destinationCup == null) {
-            destinationValue = getDestinationValue(destinationValue - 1, cupsSize)
-            destinationCup = cups.firstOrNull { it == destinationValue }
+    private fun getStringRepresentation(cups: Cups): String {
+        var current = cups[1]
+        var str = ""
+        repeat(cups.size - 1) {
+            str += current
+            current = cups[current]
         }
 
-        return cups.indexOf(destinationCup) + 1
+        return str
+    }
+}
+
+object Task2 {
+    fun getResult(input: String): Long {
+        val inputCups = input.map { it.toString().toInt() }
+        val cups = getCups(createInitialCupsMap(inputCups), inputCups[0], 10_000_000)
+        return (cups[1]?.toLong() ?: 0) * (cups[cups[1]]?.toLong() ?: 0)
     }
 
-    private fun getDestinationValue(cup: Cup, cupsSize: Int): Int {
-        var destinationValue = Math.floorMod(cup, cupsSize)
-        if (destinationValue == 0) {
-            destinationValue = cupsSize
+    private fun createInitialCupsMap(inputCups: List<Int>): Cups {
+        val cups = mutableMapOf<Int, Int>()
+
+        for (i in 0 until inputCups.size - 1) {
+            cups[inputCups[i]] = inputCups[i + 1]
         }
 
-        return destinationValue
+        // Fix last item in input
+        cups[inputCups[inputCups.size - 1]] = inputCups.size + 1
+
+        for (i in inputCups.size + 1 until 1_000_000) {
+            cups[i] = i + 1
+        }
+
+        // Fix last item
+        cups[1_000_000] = inputCups[0]
+
+        return cups
     }
+}
+
+private fun getCups(cups: Cups, startCup: Cup, iterations: Int): Cups {
+    val cupMap = cups.toMutableMap()
+    var currentCup = startCup
+
+    repeat(iterations) {
+        var value = currentCup
+
+        val cupToRemove1 = cupMap[value] ?: throw IllegalArgumentException("Could not find cup at $value")
+        val cupToRemove2 = cupMap[cupToRemove1] ?: throw IllegalArgumentException("Could not find cup at $cupToRemove1")
+        val cupToRemove3 = cupMap[cupToRemove2] ?: throw IllegalArgumentException("Could not find cup at $cupToRemove2")
+
+        do {
+            value--
+            if (value == 0) {
+                value = cupMap.size
+            }
+        } while (value == cupToRemove1 || value == cupToRemove2 || value == cupToRemove3)
+
+        cupMap[currentCup] = cupMap[cupToRemove3] ?: throw IllegalArgumentException("Could not find cup at $cupToRemove3")
+        cupMap[cupToRemove3] = cupMap[value] ?: throw IllegalArgumentException("Could not find cup at $value")
+        cupMap[value] = cupToRemove1
+
+        currentCup = cupMap[currentCup] ?: throw IllegalArgumentException("Could not find cup at $currentCup")
+    }
+
+    return cupMap
 }
--- src/test/kotlin/adventofcode/year2020/day23/Day23Test.kt
+++ src/test/kotlin/adventofcode/year2020/day23/Day23Test.kt
@@ -10,4 +10,10 @@ class Day23Test {
         val input = FileReader.getResource("year2020/day23.txt")
         println("2020 Day 23 Task 1: ${Task1.getCupLabels(input)}")
     }
+
+    @Test
+    fun testDay23Task2() {
+        val input = FileReader.getResource("year2020/day23.txt")
+        println("2020 Day 23 Task 2: ${Task2.getResult(input)}")
+    }
 }

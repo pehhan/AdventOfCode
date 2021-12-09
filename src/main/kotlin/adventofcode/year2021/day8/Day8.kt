8bbd42007d865cb44bd7aa8a10496a4be3e1e770
Pehr Hansson <pehr.hansson@parkster.se>
2021-12-09 15:24:27 +0100

    Day 8 part 2.

--- src/main/kotlin/adventofcode/year2021/day8/Day8.kt
+++ src/main/kotlin/adventofcode/year2021/day8/Day8.kt
@@ -1,25 +1,42 @@
 package adventofcode.year2021.day8
 
-typealias Pattern = String
-typealias OutputValue = String
-
-data class Entry(val signalPatterns: List<Pattern>, val outputValues: List<OutputValue>)
-
 object Task1 {
     fun occurrences(lines: List<String>): Int {
         return lines
-            .map { Entry(it.substringBefore(" | ").split(" "), it.substringAfter(" | ").split(" ")) }
-            .flatMap { it.outputValues }
+            .flatMap { it.substringAfter(" | ").split(" ") }
             .count { it.length == 2 || it.length == 3 || it.length == 4 || it.length == 7 }
     }
 }
 
 object Task2 {
     fun sumOfOutputValues(lines: List<String>): Int {
-        val entries = lines.map { Entry(it.substringBefore(" | ").split(" "), it.substringAfter(" | ").split(" ")) }
-
-        println(entries)
-
-        return 1
+        return lines
+            .asSequence()
+            .map { it.substringBefore(" | ").split(" ") to it.substringAfter(" | ").split(" ") }
+            .map { entry -> mapPattern(entry.first.map { it.toSet() }.groupBy { it.size }) to entry.second }
+            .map { entry -> entry.second.map { entry.first.indexOf(it.toSet()) } }
+            .sumOf { it.joinToString("").toInt() }
     }
+}
+
+private fun mapPattern(pattern: Map<Int, List<Set<Char>>>): List<Set<Char>> {
+    val result = MutableList(10) { setOf<Char>() }
+
+    // The easy ones
+    result[1] = pattern[2]!!.first()
+    result[4] = pattern[4]!!.first()
+    result[7] = pattern[3]!!.first()
+    result[8] = pattern[7]!!.first()
+
+    // With 5 segments
+    result[3] = pattern[5]!!.first { it.containsAll(result[1]) }
+    result[2] = pattern[5]!!.first { it + result[4] == result[8] }
+    result[5] = pattern[5]!!.first { it != result[3] && it != result[2] }
+
+    // With 6 segments
+    result[9] = pattern[6]!!.first { it.containsAll(result[3]) }
+    result[0] = pattern[6]!!.first { it.containsAll(result[1]) && it != result[9] }
+    result[6] = pattern[6]!!.first { it != result[9] && it != result[0] }
+
+    return result
 }

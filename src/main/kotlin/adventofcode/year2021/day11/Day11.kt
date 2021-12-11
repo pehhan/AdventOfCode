bc7a786b346e5a677d954f7e40675a35b56df8de
Pehr Hansson <pehr.hansson@parkster.se>
2021-12-11 14:09:13 +0100

    Day 11 part 2 2021

--- src/main/kotlin/adventofcode/year2021/day11/Day11.kt
+++ src/main/kotlin/adventofcode/year2021/day11/Day11.kt
@@ -4,31 +4,34 @@ data class Octopus(var energyLevel: Int, var hasFlashed: Boolean = false)
 
 data class Grid(private val octopi: List<List<Octopus>>) {
 
+    val size: Int
+        get() = octopi.flatten().size
+
+    var flashes = 0
     var totalFlashes = 0
 
-    fun tick(): Grid {
+    fun tick() {
+        flashes = 0
+
         increaseEnergyLevel()
         flash()
         resetEnergyLevel()
-
-        return this
     }
 
     private fun increaseEnergyLevel() {
-        octopi
-            .flatten()
-            .forEach { it.energyLevel++ }
+        octopi.flatten().forEach { it.energyLevel++ }
     }
 
     private fun flash() {
         while (anyFlashersLeft()) {
             for (y in octopi.indices) {
                 for (x in octopi[y].indices) {
-                    val octopus = octopi[x][y]
+                    val octopus = octopi[y][x]
                     if (octopus.energyLevel > 9 && !octopus.hasFlashed) {
                         flashNeighbors(x, y)
 
                         octopus.hasFlashed = true
+                        flashes++
                         totalFlashes++
                     }
                 }
@@ -44,11 +47,11 @@ data class Grid(private val octopi: List<List<Octopus>>) {
         val dx = x - 1..x + 1
         val dy = y - 1..y + 1
 
-        for (neighborX in dx) {
-            for (neighborY in dy) {
+        for (neighborY in dy) {
+            for (neighborX in dx) {
                 if (neighborX < 0 || neighborY < 0 || neighborX > octopi[0].size - 1 || neighborY > octopi.size - 1) continue
 
-                octopi[neighborX][neighborY].energyLevel++
+                octopi[neighborY][neighborX].energyLevel++
             }
         }
     }
@@ -62,38 +65,36 @@ data class Grid(private val octopi: List<List<Octopus>>) {
                 it.hasFlashed = false
             }
     }
-
-    override fun toString(): String {
-        var str = ""
-
-        for (i in octopi.indices) {
-            for (j in octopi[i].indices) {
-                str += octopi[i][j].energyLevel
-            }
-            str += "\n"
-        }
-
-        return str
-    }
 }
 
 object Task1 {
     fun totalFlashes(lines: List<String>, steps: Int): Int {
-        val octopi = lines
-            .map { it.toList() }
-            .map { list -> list.map { Octopus(it.digitToInt()) } }
-
-        var grid = Grid(octopi)
+        val grid = toGrid(lines)
 
         repeat(steps) {
-            grid = grid.tick()
-            println("### Step ${it + 1}")
-            println(grid)
+            grid.tick()
         }
 
-        println("#####")
-        println(grid.totalFlashes)
-
-        return 1
+        return grid.totalFlashes
     }
+}
+
+object Task2 {
+    fun firstStepWhenAllFlash(lines: List<String>): Int {
+        val grid = toGrid(lines)
+        var step = 0
+
+        while (grid.flashes != grid.size) {
+            grid.tick()
+            step++
+        }
+
+        return step
+    }
+}
+
+private fun toGrid(lines: List<String>): Grid {
+    return Grid(lines
+        .map { it.toList() }
+        .map { list -> list.map { Octopus(it.digitToInt()) } })
 }
--- src/test/kotlin/adventofcode/year2021/day11/Day11Test.kt
+++ src/test/kotlin/adventofcode/year2021/day11/Day11Test.kt
@@ -11,9 +11,9 @@ class Day11Test {
         println("2021 Day 11 Task 1: ${Task1.totalFlashes(lines, 100)}")
     }
 
-//    @Test
-//    fun testDay11Task2() {
-//        val lines = FileReader.getResource("year2021/day11.txt").lines()
-//        println("2021 Day 11 Task 2: ${Task2.totalFlashes(lines, 100)}")
-//    }
+    @Test
+    fun testDay11Task2() {
+        val lines = FileReader.getResource("year2021/day11.txt").lines()
+        println("2021 Day 11 Task 2: ${Task2.firstStepWhenAllFlash(lines)}")
+    }
 }

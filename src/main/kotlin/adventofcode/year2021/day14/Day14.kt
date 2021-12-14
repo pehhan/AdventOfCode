6849926eba40075a298a8cfb6d1bb3ad067f0660
Pehr Hansson <pehr.hansson@parkster.se>
2021-12-14 14:12:37 +0100

    Day 14 Part 2

--- src/main/kotlin/adventofcode/year2021/day14/Day14.kt
+++ src/main/kotlin/adventofcode/year2021/day14/Day14.kt
@@ -1,38 +1,45 @@
 package adventofcode.year2021.day14
 
-fun result(input: String, steps: Int): Int {
-    val (template, rawRules) = input.split("\n\n")
+import kotlin.math.max
 
+fun result(input: String, steps: Int): Long {
+    val (template, rawRules) = input.split("\n\n")
     val rules = parseRules(rawRules)
-    var polymer = template
+    val occurrences = template.groupingBy { it }.eachCount().mapValues { it.value.toLong() }.toMutableMap()
+    var pairs = template.windowed(2).groupingBy { it }.eachCount().mapValues { it.value.toLong() }
+
+    val newPairs = pairs.toMutableMap()
 
     repeat(steps) {
-        polymer = processRules(polymer, rules)
-        println("Step : $it")
-    }
+        for (pair in pairs) {
+            if (pair.value > 0) {
+                val replacement = rules[pair.key] ?: continue
 
-    val occurrences = polymer.groupingBy { it }.eachCount()
+                occurrences[replacement] = (occurrences[replacement] ?: 0) + pair.value
+
+                val key1 = "${pair.key.first()}$replacement"
+                val key2 = "$replacement${pair.key.last()}"
+
+                newPairs[key1] = (newPairs[key1] ?: 0) + pair.value
+                newPairs[key2] = (newPairs[key2] ?: 0) + pair.value
+
+                newPairs[pair.key] = max((newPairs[pair.key] ?: 0) - pair.value, 0)
+            }
+        }
+
+        pairs = newPairs.toMutableMap()
+    }
 
     return occurrences.maxOf { it.value } - occurrences.minOf { it.value }
 }
 
-private fun parseRules(input: String): Map<String, String> {
-    val rules = mutableMapOf<String, String>()
+private fun parseRules(input: String): Map<String, Char> {
+    val rules = mutableMapOf<String, Char>()
 
     for (line in input.lines()) {
         val (from, to) = line.split(" -> ")
-        rules[from] = to
+        rules[from] = to.first()
     }
 
     return rules
-}
-
-private fun processRules(template: String, rules: Map<String, String>): String {
-    val polymer = template
-        .windowed(2)
-        .fold("") { p, element ->
-            p + element.first() + rules[element]
-        }
-
-    return polymer + template.last()
 }
--- src/test/kotlin/adventofcode/year2021/day14/Day14Test.kt
+++ src/test/kotlin/adventofcode/year2021/day14/Day14Test.kt
@@ -14,6 +14,6 @@ class Day14Test {
     @Test
     fun testDay14Task2() {
         val input = FileReader.getResource("year2021/day14.txt")
-        println("2021 Day 14 Task 2:\n${result(input, 40)}")
+        println("2021 Day 14 Task 2: ${result(input, 40)}")
     }
 }

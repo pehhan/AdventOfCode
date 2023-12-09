28fd54ee32530fb4517cb02990edafcda58b985e
Pehr Hansson <pehr.hansson@parkster.se>
2023-12-09 14:03:01 +0100

    feat: 2023 day 8 task 2 fixed

--- src/main/kotlin/adventofcode/year2023/day8/Day8.kt
+++ src/main/kotlin/adventofcode/year2023/day8/Day8.kt
@@ -9,11 +9,11 @@ typealias Node = String
 typealias NodeMap = Map<Node, Pair<Node, Node>>
 
 object Task1 {
-    fun steps(input: String): Int {
+    fun steps(input: String): Long {
         val instructions = input.toInstructions()
         val nodeMap = input.toNodeMap()
 
-        return steps(instructions, nodeMap)
+        return steps(instructions, nodeMap, startNode = "AAA", endNodeSuffix = "ZZZ")
     }
 }
 
@@ -21,22 +21,47 @@ object Task2 {
     fun steps(input: String): Long {
         val instructions = input.toInstructions()
         val nodeMap = input.toNodeMap()
-
         val startNodes = nodeMap.map { it.key }.filter { it.last() == 'A' }
-
-        println(startNodes)
 
         return steps(instructions, nodeMap, startNodes)
     }
+
+    private fun steps(instructions: List<Instruction>, nodeMap: NodeMap, startNodes: List<Node>): Long {
+        val stepList = startNodes.fold(emptyList<Long>()) { acc, node ->
+            acc + steps(instructions, nodeMap, startNode = node, endNodeSuffix = "Z")
+        }
+
+        return leastCommonMultiplier(stepList)
+    }
+
+    private fun leastCommonMultiplier(numbers: List<Long>): Long {
+        var result = numbers[0]
+        for (i in 1 until numbers.size) {
+            result = leastCommonMultiplier(result, numbers[i])
+        }
+        return result
+    }
+
+    private fun leastCommonMultiplier(a: Long, b: Long): Long {
+        val larger = if (a > b) a else b
+        val maxLcm = a * b
+        var lcm = larger
+        while (lcm <= maxLcm) {
+            if (lcm % a == 0L && lcm % b == 0L) {
+                return lcm
+            }
+            lcm += larger
+        }
+        return maxLcm
+    }
 }
 
-private fun steps(instructions: List<Instruction>, nodeMap: NodeMap): Int {
-    var steps = 0
+private fun steps(instructions: List<Instruction>, nodeMap: NodeMap, startNode: Node, endNodeSuffix: String): Long {
+    var steps = 0L
     var instructionIndex = 0
-    var node = "AAA"
-    val endNode = "ZZZ"
+    var node = startNode
 
-    while (node != endNode) {
+    while (!node.endsWith(endNodeSuffix)) {
         val instruction = instructions[instructionIndex]
         val map = nodeMap[node] ?: break
 
@@ -50,44 +75,6 @@ private fun steps(instructions: List<Instruction>, nodeMap: NodeMap): Int {
     }
 
     return steps
-}
-
-private fun steps(instructions: List<Instruction>, nodeMap: NodeMap, startNodes: List<Node>): Long {
-    var steps = 0L
-    var instructionIndex = 0
-    var nodes = startNodes.toMutableList()
-
-    while (!nodes.areAllNodesEndNodes()) {
-        val instruction = instructions[instructionIndex]
-        val newNodes = mutableListOf<Node>()
-
-        for (node in nodes) {
-            val map = nodeMap[node] ?: break
-
-            val newNode = when (instruction) {
-                Instruction.Left -> map.first
-                Instruction.Right -> map.second
-            }
-
-            newNodes += newNode
-        }
-
-        nodes = newNodes
-
-        instructionIndex = (instructionIndex + 1) % instructions.size
-        steps++
-
-        if (steps % 10_0000_000 == 0L) {
-            println(nodes)
-            println(steps)
-        }
-    }
-
-    return steps
-}
-
-private fun List<Node>.areAllNodesEndNodes(): Boolean {
-    return this.all { it.last() == 'Z' }
 }
 
 private fun String.toInstructions(): List<Instruction> {

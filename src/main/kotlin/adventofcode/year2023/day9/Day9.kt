878b975c67048fe2ee6a7fe6207f9c99b8b8d354
Pehr Hansson <pehr.hansson@parkster.se>
2023-12-09 13:39:03 +0100

    feat: 2023 day 9 task 2

--- src/main/kotlin/adventofcode/year2023/day9/Day9.kt
+++ src/main/kotlin/adventofcode/year2023/day9/Day9.kt
@@ -2,35 +2,47 @@ package adventofcode.year2023.day9
 
 object Task1 {
     fun sum(input: String): Int {
-        val lines = input
-            .lines()
-            .map { it.split(" ") }
-            .map { list -> list.map { it.toInt() } }
-
-        return sumOfExtrapolatedValues(lines)
-    }
-
-    private fun sumOfExtrapolatedValues(lines: List<List<Int>>): Int {
-        var sum = 0
-
-        for (line in lines) {
-            val lists = mutableListOf<List<Int>>()
-            lists += line
-
-            var newList = line
-
-            while (!newList.all { it == 0 }) {
-                newList = newList.windowed(2).map { it.last() - it.first() }
-                lists += newList
-            }
-
-            sum += extrapolateLastValue(lists)
-        }
-
-        return sum
+        val lines = parseToLines(input)
+        return sumOfExtrapolatedValues(lines, ::extrapolateLastValue)
     }
 
     private fun extrapolateLastValue(lists: List<List<Int>>): Int {
         return lists.sumOf { it.last() }
+    }
+}
+
+object Task2 {
+    fun sum(input: String): Int {
+        val lines = parseToLines(input)
+        return sumOfExtrapolatedValues(lines, ::extrapolateFirstValue)
+    }
+
+    private fun extrapolateFirstValue(lists: List<List<Int>>): Int {
+        return lists.reversed().drop(1).fold(0) { acc, values ->
+            values.first() - acc
+        }
+    }
+}
+
+private fun parseToLines(input: String): List<List<Int>> {
+    return input
+        .lines()
+        .map { it.split(" ") }
+        .map { list -> list.map { it.toInt() } }
+}
+
+private fun sumOfExtrapolatedValues(lines: List<List<Int>>, extrapolateFunc: (List<List<Int>>) -> Int): Int {
+    return lines.fold(0) { acc, line ->
+        val lists = mutableListOf<List<Int>>()
+        lists += line
+
+        var newList = line
+
+        while (!newList.all { it == 0 }) {
+            newList = newList.windowed(2).map { it.last() - it.first() }
+            lists += newList
+        }
+
+        acc + extrapolateFunc(lists)
     }
 }
--- src/test/kotlin/adventofcode/year2023/day9/Day9Test.kt
+++ src/test/kotlin/adventofcode/year2023/day9/Day9Test.kt
@@ -11,9 +11,9 @@ class Day9Test {
         println("2023 Day 9 Task 1: ${Task1.sum(input)}")
     }
 
-//    @Test
-//    fun testDay9Task2() {
-//        val input = FileReader.getResource("year2023/day9.txt")
-//        println("2023 Day 9 Task 2: ${Task2.steps(input)}")
-//    }
+    @Test
+    fun testDay9Task2() {
+        val input = FileReader.getResource("year2023/day9.txt")
+        println("2023 Day 9 Task 2: ${Task2.sum(input)}")
+    }
 }

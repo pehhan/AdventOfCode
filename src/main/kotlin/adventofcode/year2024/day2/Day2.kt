99cbca2502a3cf45221569397b9136cd69e9c056
Pehr Hansson <pehr.hansson@parkster.se>
2024-12-07 11:47:15 +0100

    chore: Cleanup

--- src/main/kotlin/adventofcode/year2024/day2/Day2.kt
+++ src/main/kotlin/adventofcode/year2024/day2/Day2.kt
@@ -20,24 +20,23 @@ object Task2 {
     fun safeReports(input: String): Int {
         return input
             .lines()
+            .asSequence()
             .map { it.split(" ") }
             .map { list -> list.map { it.toInt() } }
             .map { list ->
-                list
-                    .mapIndexed { index, _ ->
-                        val from = max(0, index)
-                        val to = min(list.size, index + 1)
-                        list.subList(0, from) + list.subList(to, list.size)
-                    }
-                    .map { it.zipWithNext { a, b -> a - b } }
+                List(list.size) { index ->
+                    val from = max(0, index)
+                    val to = min(list.size, index + 1)
+                    list.subList(0, from) + list.subList(to, list.size)
+                }.map { it.zipWithNext { a, b -> a - b } }
             }
             .map { lists ->
                 lists.map { list ->
                     list.isSafe()
                 }
             }
-            .count {
-                it.any { it }
+            .count { list ->
+                list.any { it }
             }
     }
 }
